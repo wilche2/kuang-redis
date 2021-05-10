2535710999ac13c0d7ed30d4c297c81019abdb01
wilche <154490984@qq.com>
2021-05-10 09:20:31 +0800

    springboot 整合redis集群（主从+哨兵）

--- redis-02-springboot/src/test/java/com/kuang/Redis02SpringbootApplicationTests.java
+++ redis-02-springboot/src/test/java/com/kuang/Redis02SpringbootApplicationTests.java
@@ -1,10 +1,7 @@
 package com.kuang;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kuang.pojo.User;
-import com.kuang.utils.RedisUtil;
-import lombok.AllArgsConstructor;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,24 +10,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
-import javax.annotation.Resource;
-
 @SpringBootTest
 class Redis02SpringbootApplicationTests {
 
     @Autowired
-    @Qualifier("redisTemplate2")
+    @Qualifier("redisTemplate")
     private RedisTemplate redisTemplate2;
 
-    @Autowired
-    private RedisUtil redisUtil;
+//    @Autowired
+//    private RedisUtil redisUtil;
 
     @Test
     void contextLoads() throws JSONException, JsonProcessingException {
 
 //        RedisConnection connection = redisTemplate.getConnectionFactory().getConnection();
 //        connection.flushAll();
-
         ValueOperations valueOperations = redisTemplate2.opsForValue();
 
         User u = new User();
@@ -38,16 +32,16 @@ class Redis02SpringbootApplicationTests {
         // String s = new ObjectMapper().writeValueAsString(u);
 
         // 从redis-cli去看，会发现是转义过后的数据，是因为序列化的缘故
-        valueOperations.set("ddd", u);
-        System.out.println("valueOperations.get(\"ddd\") = " + valueOperations.get("ddd"));
+        valueOperations.set("test1", u);
+        System.out.println("valueOperations.get(\"test1\") = " + valueOperations.get("test1"));
     }
 
-    @Test
-    void testRedisUtil() throws JsonProcessingException {
-        User u = new User();
-        u.setName("777777777777777777");
-        redisUtil.set("user", new ObjectMapper().writeValueAsString(u));
-        System.out.println("redisUtil.get(\"user\") = " + redisUtil.get("user"));
-    }
+//    @Test
+//    void testRedisUtil() throws JsonProcessingException {
+//        User u = new User();
+//        u.setName("777777777777777777");
+//        redisUtil.set("user", new ObjectMapper().writeValueAsString(u));
+//        System.out.println("redisUtil.get(\"user\") = " + redisUtil.get("user"));
+//    }
 
 }
